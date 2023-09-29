53b16a88cb5f00d8edce10badd00d4bc20103aa5
Vivek Iyer Vaidyanathan <vvivekiyer@gmail.com>
2023-09-28 22:51:38 -0700

    Add thresholds to limit query response size at server

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/OfflineClusterIntegrationTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/OfflineClusterIntegrationTest.java
@@ -584,6 +584,16 @@ public class OfflineClusterIntegrationTest extends BaseClusterIntegrationTestSet
   }
 
   @Test
+  public void testMaxSerializedResponseLengthPerServer()
+      throws Exception {
+    String queryWithOption = "SET maxSerializedResponseLengthPerServer=1000; " + SELECT_STAR_QUERY;
+    JsonNode response = postQuery(queryWithOption);
+    assert response.get("exceptions").size() > 0;
+    String exceptionStr = response.get("exceptions").get(0).get("message").toString();
+    assert exceptionStr.contains("Serialized response exceeds threshold");
+  }
+
+  @Test
   public void testRegexpReplace()
       throws Exception {
     // Correctness tests of regexpReplace.
